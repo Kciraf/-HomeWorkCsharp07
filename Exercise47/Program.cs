72660c579355a7f5c745df86b75690f50de02727
Kciraf <valsinat44a@mail.ru>
2023-03-11 15:32:59 +0600

    Added Ex47

--- Exercise47/Program.cs
+++ Exercise47/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+double[,] CreateMatrix(int m, int n)
+{
+    double[,] matrix = new double[m, n];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(0, 10) + Math.Round(rnd.NextDouble(), 2); // Генерация случайного double числа (округление до 2х знаков после запятой)
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write(matrix[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+System.Console.WriteLine("Задайте размер матрицы (строки <пробел> столбцы): ");
+int[] size = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+
+PrintMatrix(CreateMatrix(size[0], size[1]));
